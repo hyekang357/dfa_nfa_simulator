25608e95646c8ead691fef15133b904c33be5848
hyekang357 <hyekang357@gmail.com>
2018-09-21 16:50:42 -0400

    added draw-functions

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -25,12 +25,27 @@ public class Controller {
     public TextArea TextAreaOutput;
     public VBox VBoxForCanvas;
 
+    private void drawAcceptState(GraphicsContext gc, int x, int y, int size) {
+        gc.strokeOval(x, y, size, size);
+        gc.strokeOval(x-8, y-8, size+16, size+16);
+    }
+
+    private void drawRejectState(GraphicsContext gc, int x, int y, int size) {
+        gc.strokeOval(x, y, size, size);
+    }
+
+    private void drawTransistionToItself(GraphicsContext gc, int x, int y, int size) {
+        gc.strokeArc(x+5, y-78, size, size, 320, 260, ArcType.OPEN);
+        Arrow aa = new Arrow(true, x+5, y-78);
+        gc.fillPolygon(aa.get_x_points(), aa.get_y_points(), aa.get_num_points());
+    }
+
     public void drawTest1() {
 
         Canvas c = new Canvas(918.0, 483.0);
         GraphicsContext GC = c.getGraphicsContext2D();
 
-        System.out.println("hello");
+        System.out.println("hello...");
 
         GC.strokeText("TL", 0, 10);
         GC.strokeText("TR", 900, 10);
@@ -39,42 +54,28 @@ public class Controller {
 
         GC.setLineWidth(3.0);
 
+        //q0
+        drawAcceptState(GC, 208, 208, 100);
+        drawTransistionToItself(GC, 208, 208, 100);
+        //q1
+        drawAcceptState(GC, 408, 208, 100);
+        drawTransistionToItself(GC, 408, 208, 100);
+        // q2
+        drawRejectState(GC,608, 208, 100);
+        drawTransistionToItself(GC, 608, 208, 100);
+
+        // draw straight lines with arrows
         GC.strokeLine(100, 258, 200, 258);
         Arrow sa1 = new Arrow(false, 200, 258);
         GC.fillPolygon(sa1.get_x_points(), sa1.get_y_points(), sa1.get_num_points());
-
-        //q0
-        GC.strokeOval(208, 208, 100, 100);
-        GC.strokeOval(200, 200, 116, 116);
 
         GC.strokeLine(318, 258, 400, 258);
         Arrow sa2 = new Arrow(false, 400, 258);
         GC.fillPolygon(sa2.get_x_points(), sa2.get_y_points(), sa2.get_num_points());
 
-        //q1
-        GC.strokeOval(408, 208, 100, 100);
-        GC.strokeOval(400, 200, 116, 116);
-
         GC.strokeLine(516, 258, 607, 258);
         Arrow sa3 = new Arrow(false, 607, 258);
         GC.fillPolygon(sa3.get_x_points(), sa3.get_y_points(), sa3.get_num_points());
-
-        // q2
-        GC.strokeOval(608, 208, 100, 100);
-
-        // transition to itself
-        GC.strokeArc(210, 130, 100, 100, 320, 260, ArcType.OPEN);
-        Arrow aa1 = new Arrow(true, 210, 130);
-        GC.fillPolygon(aa1.get_x_points(), aa1.get_y_points(), aa1.get_num_points());
-
-
-        GC.strokeArc(410, 130, 100, 100, 320, 260, ArcType.OPEN);
-        Arrow aa2 = new Arrow(true, 410, 130);
-        GC.fillPolygon(aa2.get_x_points(), aa2.get_y_points(), aa2.get_num_points());
-
-        GC.strokeArc(610, 140, 100, 100, 320, 260, ArcType.OPEN);
-        Arrow aa3 = new Arrow(true, 610, 140);
-        GC.fillPolygon(aa3.get_x_points(), aa3.get_y_points(), aa3.get_num_points());
 
 
         VBoxForCanvas.getChildren().remove(0);
